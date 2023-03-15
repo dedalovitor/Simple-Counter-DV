8d3f4b49317725b3d9c1383008d12f928eef5719
dedalovitor <dedalovitor@gmail.com>
2023-03-15 05:26:39 +0000

    uploading my code

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,34 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
+
 const Home = () => {
+	const [timer, setTimer] = useState(0);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setTimer((x) => x + 1);
+			console.log(timer);
+		}, 1000);
+
+		return () => clearInterval(interval); // This represents the unmount function, in which you need to clear your interval to prevent memory leaks.
+	}, []);
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="p-5 bg-dark container">
+			<div className="d-flex flex-row justify-content-center">
+				<div className="clock text-white">
+					<i
+						className="fa-regular fa-clock fa-spin p-2">
+					</i>
+				</div>
+				<div className="four text-white p-2 fs-1">{Math.floor(timer / 1000) % 10}</div>
+				<div className="three text-white p-2 fs-1">{Math.floor(timer / 100) % 10}</div>
+				<div className="two text-white p-2 fs-1">{Math.floor(timer / 10) % 10}</div>
+				<div className="one text-white p-2 fs-1">{Math.floor(timer / 1) % 10}</div>
+			</div>
 		</div>
 	);
 };
